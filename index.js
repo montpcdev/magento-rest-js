2cf3cb49d33bf8b6f4c26f8fd1a5d4e8bcb2b771
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-10-01 02:44:21 +0800

    added delete method

--- index.js
+++ index.js
@@ -18,11 +18,34 @@ class MagentoClient {
     this.token = token
   }
 
+
+  /**
+   *
+   *
+   * @param {*} params
+   * @return {*} 
+   * @memberof MagentoClient
+   */
   productsList (params) {
     const options = {
       headers: { Authorization: `Bearer ${this.token}` },
     }
     return requestPromise.get(`https://${this.url}/rest/default/V1/products?${querystring.stringify(params)}`, options)
+      .then(res => JSON.parse(res.body))
+  }
+
+  /**
+   *
+   *
+   * @param {*} sku
+   * @return {*} 
+   * @memberof MagentoClient
+   */
+  productsDelete (sku) {
+    const options = {
+      headers: { Authorization: `Bearer ${this.token}` },
+    }
+    return requestPromise.delete(`https://${this.url}/rest/default/V1/products/${sku}`)
       .then(res => JSON.parse(res.body))
   }
 }
